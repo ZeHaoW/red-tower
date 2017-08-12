82e6ffeb05150a15855a0a58390af7787794a077
zehaowang <zehaowang@163.com>
2017-08-12 19:46:22 +0800

    2.0版本

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,11 +1,36 @@
+
 function showPic(whichpic){
+if(!document.getElementById('placeholder')) return false;
 document.getElementById('placeholder').setAttribute("src", whichpic.getAttribute("href"));
-document.getElementById('description').firstChild.nodeValue = whichpic.getAttribute("title");
+if(document.getElementById('description')){
+  document.getElementById('description').firstChild.nodeValue = whichpic.getAttribute("title");
+}
+return true;
 }
 
-function countBodyChildren(){
-var body_element = document.getElementsByTagName("body")[0];
-alert(body_element.childNodes.length);
+
+function prepareGallery(){
+  if(!document.getElementsByTagName || !document.getElementById) return false;
+  if(!document.getElementById('imagegallery')) return false;
+  var links = document.getElementById('imagegallery').getElementsByTagName("a");
+  for(var i = 0; i < links.length; i++){
+    links[i].onclick = function(){
+      return !showPic(this);
+    }
+  }
 }
 
-window.onload = countBodyChildren;
+function addLoadEvent(func){
+  var oldonload = window.onload;
+  if(typeof window.onload != 'function'){
+    window.onload = func;
+  }
+  else {
+    window.onload = function(){
+      oldonload();
+      func();
+    }
+  }
+}
+
+addLoadEvent(prepareGallery);
